97987970eab746d5a548022125e19d6df011fbe3
pglah <p.glahe@gmx.de>
2021-03-10 14:39:42 +0100

    darknet: Test upload model.

--- darknet_trainer/darknet_tests/test_update_model.py
+++ darknet_trainer/darknet_tests/test_update_model.py
@@ -37,13 +37,24 @@ def test_parse_latest_confusion_matrix():
 
 @pytest.mark.asyncio
 async def test_model_is_updated():
-    
-    #TODO check model count before and after
-    
+    def get_box_categories():
+        content = test_helper.LiveServerSession().get('/api/zauberzeug/projects/pytest/data').json()
+        categories = content['box_categories']
+        return categories
+
+    def get_model_ids_from__latest_training():
+        content = test_helper.LiveServerSession().get('/api/zauberzeug/projects/pytest/trainings')
+        content_json = content.json()
+        datapoints = [datapoint['model_id'] for datapoint in content_json['charts'][0]['data']]
+        return datapoints
+
     await main.node.connect()
     await main.node.sio.sleep(1.0)
 
     model_id = test_helper.assert_upload_model()
+
+    model_ids = get_model_ids_from__latest_training()
+    assert(len(model_ids)) == 1
 
     training_uuid = "some_uuid"
     _, _, training_path = test_helper.create_needed_folders(training_uuid)
@@ -55,7 +66,10 @@ async def test_model_is_updated():
                               'training_id': training_uuid}
     main.node.status.train_images = []
     main.node.status.test_images = []
+    main.node.status.box_categories = get_box_categories()
 
     assert main.node.status.model.get('last_published_iteration') == None
     await main._check_state()
     assert main.node.status.model.get('last_published_iteration') == 1089
+    model_ids = get_model_ids_from__latest_training()
+    assert(len(model_ids)) == 2
