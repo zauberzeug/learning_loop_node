cfa647d9c116c394fb7ead34a5b9d90164b641ac
Maik Switalski <maik@zauberzeug.com>
2021-03-12 15:14:44 +0100

    darknet, update_model: replace existing model with new one. Keep training_id.

--- darknet_trainer/main.py
+++ darknet_trainer/main.py
@@ -202,6 +202,8 @@ async def _check_for_new_model(training_id: str) -> None:
                 'trainer_id': node.status.id,
             }
             await node.sio.call('update_model', (node.status.organization, node.status.project, new_model))
+            node.status.model = new_model
+            node.status.model['training_id'] = training_id
             node.status.model['last_published_iteration'] = model['iteration']
 
 
