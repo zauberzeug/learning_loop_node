571edf0a279179650ac635cbc7eca080fe783071
pglah <p.glahe@gmx.de>
2021-03-31 16:52:15 +0200

    active_learner: Send detections  for active learning to loop.

--- detection_node/main.py
+++ detection_node/main.py
@@ -14,6 +14,7 @@ from fastapi_utils.tasks import repeat_every
 from active_learner import learner as l
 import json
 from active_learner import detection as d
+import requests
 
 node = Node(uuid='12d7750b-4f0c-4d8d-86c6-c5ad04e19d57', name='detection node')
 node.path = '/model'
@@ -57,7 +58,8 @@ async def compute_detections(request: Request, file: UploadFile = File(...), mac
     return JSONResponse({'box_detections': detections})
 
 
-@repeat_every(seconds=5, raise_exceptions=True, wait_first=True)
+@node.on_event("startup")
+@repeat_every(seconds=30, raise_exceptions=False, wait_first=False)
 async def handle_detections() -> None:
     files_for_active_learning = glob('/data/*', recursive=True)
 
@@ -66,19 +68,21 @@ async def handle_detections() -> None:
 
     if files_for_active_learning:
         macs_dict = helper.extract_macs_and_filenames(files_for_active_learning)
-        for mac, filename in macs_dict.items():
+        for mac, filenames in macs_dict.items():
             if mac not in learners:
                 learners[mac] = l.Learner()
 
-            with open(f'/data/{filename}.json') as f:
-                detections = json.load(f)
+            for filename in filenames:
+                with open(f'/data/{filename}.json') as f:
+                    detections = json.load(f)
 
-            active_learning_causes = learners[mac].add_detections(
-                [d.ActiveLearnerDetection(detection) for detection in detections['box_detections']])
+                active_learning_causes = learners[mac].add_detections(
+                    [d.ActiveLearnerDetection(detection['category_name'], detection['x'], detection['y'], detection['width'], detection['height'], detection['model_name'], detection['confidence']) for detection in detections['box_detections']])
 
-            if any(active_learning_causes):
-                # Send image with detection to LL
-                pass
+                if any(active_learning_causes):
+                    data = [('file', open(f'/data/{filename}.json', 'r')),
+                            ('file', open(f'/data/{filename}.jpg', 'rb'))]
+                    requests.post('http://backend/api/zauberzeug/projects/demo/images', files=data)
 
 
 node.include_router(router, prefix="")
