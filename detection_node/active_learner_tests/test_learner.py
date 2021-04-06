1b610f212bd1092209f136efbd55ff5afadba6cd
pglah <p.glahe@gmx.de>
2021-04-06 16:50:38 +0200

    active_learner: Simplify test.

--- detection_node/active_learner_tests/test_learner.py
+++ detection_node/active_learner_tests/test_learner.py
@@ -45,8 +45,9 @@ def test_add_second_detection_to_learner():
 
 
 def test_update_last_seen():
-    # TODO modify .last_seen instead.
-    time.sleep(0.5)
+    ic(dirt_detection.last_seen)
+    dirt_detection.last_seen = datetime.now() - timedelta(seconds=.5)
+    ic(dirt_detection.last_seen)
     assert dirt_detection._is_older_than(0.5) == True
     learner = l.Learner()
     learner.add_detections([dirt_detection])
@@ -106,4 +107,3 @@ def test_active_learner_extracts_from_json():
         [d.ActiveLearnerDetection(Detection.from_dict(_detection)) for _detection in detections])
 
     assert active_learning_cause == ['lowConfidence']
-    # TODO count check.
