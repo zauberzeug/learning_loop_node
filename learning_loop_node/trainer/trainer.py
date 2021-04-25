ac6d8fe36fc8345f07d906000d6c79cea3e92a98
Maik Switalski <maik@zauberzeug.com>
2021-04-25 19:37:42 +0200

    node, trainer: cleanup.

--- learning_loop_node/trainer/trainer.py
+++ learning_loop_node/trainer/trainer.py
@@ -9,6 +9,7 @@ import os
 from uuid import uuid4
 from fastapi.encoders import jsonable_encoder
 from learning_loop_node.status import Status
+from learning_loop_node.status import TrainingStatus
 
 
 class Trainer(Node):
@@ -25,10 +26,6 @@ class Trainer(Node):
                 raise Exception(msg)
 
             print(f'---- running training with source model {source_model} for {organization}.{project}', flush=True)
-            # self.status.model = source_model
-            # self.status.organization = organization
-            # self.status.project = project
-
             uri_base = f'{self.url}/api/{ organization}/projects/{ project}'
 
             response = requests.get(uri_base + '/data/data2?state=complete', headers=self.headers)
@@ -82,31 +79,20 @@ class Trainer(Node):
                 return response.json()['detail']
 
     async def send_status(self):
-        print("asdf")
-
-        from learning_loop_node.status import TrainingStatus
-        try:
-            status = TrainingStatus(
-                id=self.uuid,
-                name=self.name,
-                state=self.status.state,
-                uptime=self.status.uptime
-            )
-        except Exception as e:
-            print(e, flush=True)
-            print()
-
+        status = TrainingStatus(
+            id=self.uuid,
+            name=self.name,
+            state=self.status.state,
+            uptime=self.status.uptime
+        )
         if self.training:
             status.latest_produced_model_id = self.training.last_known_model.id
-            # status.organization = self.training.organization
-            # status.project = self.training.project
-            # if self.training.data:
-            #     status.box_categories = self.training.data.box_categories
 
         print('sending status', status, flush=True)
-        result = await self.sio.call('update_trainer', jsonable_encoder(status))
+        result = await self.sio.call('update_trainer', jsonable_encoder(status), timeout=1)
         if not result == True:
             raise Exception(result)
+        print('status send', flush=True)
 
     async def update_status(self, status: Status):
         self.training = None
