3cab8cca1ec59849b543ea6d7c5cd233b6b42db0
Maik Switalski <maik@zauberzeug.com>
2021-05-06 08:46:13 +0200

    node, trainer: cleanup

--- mock_trainer/main.py
+++ mock_trainer/main.py
@@ -4,7 +4,7 @@ import uvicorn
 from learning_loop_node.trainer.trainer_node import TrainerNode
 from learning_loop_node.trainer.mock_trainer import MockTrainer
 
-# TODO remove doublicated uuid and name properties.
+
 mock_trainer = MockTrainer(capability=Capability.Box)
 trainer_node = TrainerNode(uuid='85ef1a58-308d-4c80-8931-43d1f752f4f2', name='mocked trainer', trainer=mock_trainer)
 
