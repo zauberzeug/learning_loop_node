77dc73da4914eb821ec19f94bd3b2b496e1b8567
Rodja Trappe <rodja@zauberzeug.com>
2021-08-23 05:24:13 +0200

    improved test

--- learning_loop_node/tests/test_executor.py
+++ learning_loop_node/tests/test_executor.py
@@ -8,11 +8,11 @@ def test_executor_lifecycle():
     executor = Executor('/tmp/test_executor/' + str(uuid4()))
     cmd = executor.path + '/some_executable.sh'
     with open(cmd, 'w') as f:
-        f.write('while true; do ls; sleep 1; done')
+        f.write('while true; do echo "some output"; sleep 1; done')
     os.chmod(cmd, 0o755)
     executor.start(cmd)
     assert executor.is_process_running()
-    sleep(2)
-    assert 'last_training.log' in executor.get_log()
+    sleep(1)
+    assert 'some output' in executor.get_log()
     executor.stop()
     assert executor.is_process_running() == False
