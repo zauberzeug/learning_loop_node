0f2f6aee11a7d4dc2217103951d73fead47a3006
Zauberzeug on Edge Device <development@zauberzeug.com>
2021-10-29 09:12:48 +0200

    excecutor: kill process group to kill child process as well. Do not catch errors so that we can reproduce this issues and decide what to do when they occur.

--- learning_loop_node/trainer/executor.py
+++ learning_loop_node/trainer/executor.py
@@ -6,6 +6,7 @@ import signal
 from icecream import ic
 import logging
 
+
 class Executor:
 
     def __init__(self, base_path) -> None:
@@ -14,13 +15,13 @@ class Executor:
         self.process = None
 
     def start(self, cmd: str):
-        # NOTE we have to write the pid inside the bash command to get the correct pid.
         self.process = subprocess.Popen(
             f'cd {self.path}; {cmd} >> last_training.log 2>&1',
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             executable='/bin/bash',
+            preexec_fn=os.setsid
         )
 
     def is_process_running(self):
@@ -45,19 +46,10 @@ class Executor:
         if self.process is None:
             logging.info('no process running ... nothing to stop')
             return
-        try:
-            logging.info('terminating process')
-            try:
-                os.kill(self.process.pid, signal.SIGTERM)
-            except:
-                pass
-            self.process.terminate()
-            out, err = self.process.communicate(timeout=3)
-        except subprocess.TimeoutExpired:
-            try:
-                os.kill(self.process.pid, signal.SIGKILL)
-            except:
-                pass
-            logging.info('could not terminate process -- trying to kill')
-            self.process.kill()
-            out, err = self.process.communicate()
+
+        logging.info('terminating process')
+
+        os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+
+        self.process.terminate()
+        out, err = self.process.communicate(timeout=3)
