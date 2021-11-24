09fa95d7ad7f6f2ceb0c609ceaafe908c83b0f8a
Maik Switalski <maik@zauberzeug.com>
2021-11-24 18:33:49 +0100

    annotation node: draw bbox and use grabcut on this bbox

--- demo_segmentation_tool/demo_segmentation_tool.py
+++ demo_segmentation_tool/demo_segmentation_tool.py
@@ -6,20 +6,88 @@ from pydantic import BaseModel
 import numpy as np
 import cv2
 from icecream import ic
+import logging
+
+
+class Box(BaseModel):
+    x: int
+    y: int
+    w: int
+    h: int
 
 
 class History(BaseModel):
-    inside_clicks: List[Point] = []
-    outside_clicks: List[Point] = []
+    bbox: Optional[Box]
+    rect_creation_finished: bool = False
+    bg_pixel: List[Point] = []
+    fg_pixel: List[Point] = []
     annotation: Optional[SegmentationAnnotation]
+
+    def to_svg(self) -> str:
+
+        # width = abs(self.x2-self.x)
+        # height = abs(self.y2-self.y)
+        # return f'<rect x="{min(self.x, self.x2)}" y="{min(self.y, self.y2)}" width="{width or 1}" height="{height or 1}" fill="blue">'
+        return f'<rect x="{self.bbox.x}" y="{self.bbox.y}" width="{self.bbox.w}" height="{self.bbox.h}" stroke="blue" fill="transparent">'
 
 
 class DemoSegmentationTool(AnnotationTool):
     history: Optional[History]
 
     async def handle_user_input(self, user_input: UserInput) -> ToolOutput:
+        try:
+            if self.history.rect_creation_finished == True:
+                self.history = None  # // simple resetting.
+        except:
+            pass
 
-        # points = [Point(x=0, y=0), Point(x=100, y=100), Point(x=0, y=100)]
+        coordinate = user_input.data.coordinate
+        output = ToolOutput(svg="", annotation=None)
+        if not self.history and user_input.data.event_type == EventType.MouseDown:
+            self.history = History()
+            self.history.bbox = Box(x=coordinate.x, y=coordinate.y, w=0, h=0)
+            # start bbox Creation
+            output.svg = self.history.to_svg()
+            return output
+
+        elif self.history.rect_creation_finished == False and user_input.data.event_type == EventType.MouseMove:
+            # update update bbox
+            self.history.bbox.w = coordinate.x - self.history.bbox.x
+            self.history.bbox.h = coordinate.y - self.history.bbox.y
+            output.svg = self.history.to_svg()
+            return output
+            pass
+        elif self.history.rect_creation_finished == False and user_input.data.event_type == EventType.MouseUp:
+            # end update bbox
+            ic('hier')
+            self.history.bbox.w = coordinate.x - self.history.bbox.x
+            self.history.bbox.h = coordinate.y - self.history.bbox.y
+            self.history.rect_creation_finished = True
+
+            points = autofit(f'/data/{user_input.data.context.organization}/{user_input.data.context.project}/images/{user_input.data.image_uuid}.jpg',
+                             self.history)
+            self.history.annotation = SegmentationAnnotation(id=str(uuid4()), shape=Shape(points=[]),
+                                                             image_id=user_input.data.image_uuid, category_id=user_input.data.category.id)
+            self.history.annotation.shape.points = points
+            ic(points)
+            output.svg = self.history.to_svg()
+            output.annotation = self.history.annotation
+            return output
+
+        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseDown:
+            # start gathering bg vg points
+            pass
+        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseMove:
+            # gathering bg vg points
+            pass
+        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseUp:
+            # start gathering bg vg points
+            # crabcut
+            pass
+        else:
+            import logging
+            logging.error('Invalid State')
+
         try:
             ic(user_input.data.image_uuid)
             ic(self.history.annotation.image_id)
@@ -50,30 +118,38 @@ class DemoSegmentationTool(AnnotationTool):
 
 
 def autofit(image_path, history: History) -> List[Point]:
+    logging.info('inside grab cut')
     img = cv2.imread(image_path)
-
+    x_, y_, w_, h_ = int(history.bbox.x), int(history.bbox.y), int(history.bbox.w), int(history.bbox.h)
+    # // 100*100 box
     # region-of-interest
+    padding_factor = 4
     H, W = img.shape[:2]
-    ic(f'Image width, height : {W}, {H} ')
-    ic(f'ClickPoint : {Point.__dict__}')
+    roi_l = max(x_ - padding_factor * w_, 0)  # x-400
+    roi_t = max(y_ - padding_factor * h_, 0)  # y -400
+    roi_r = min(x_ + (padding_factor + 1) * w_, W)  # x+500
+    roi_b = min(y_ + (padding_factor + 1) * h_, H)  # y+500
+    crop = img[roi_t:roi_b, roi_l:roi_r]
+    crop_rect = x_ - roi_l, y_ - roi_t, w_, h_
 
-    mask = np.ones(img.shape[:2], np.uint8) * cv2.GC_BGD
+    # grabcut
+    mask = np.ones(crop.shape[:2], np.uint8) * cv2.GC_BGD
+    mask[crop_rect[1]:crop_rect[1]+crop_rect[3],
+         crop_rect[0]:crop_rect[0]+crop_rect[2]] = cv2.GC_PR_FGD
+    mask[crop_rect[1]+crop_rect[3]//4:crop_rect[1]+3*crop_rect[3]//4,
+         crop_rect[0]+crop_rect[2]//4:crop_rect[0]+3*crop_rect[2]//4] = cv2.GC_FGD
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    cv2.grabCut(img, mask, crop_rect, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK)
+    # rows, cols = np.where(mask % 2 > 0)
 
-    array_points = []
-    polygon = np.array([array_points], dtype=np.int32)
+    mask = np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 0, 1).astype(np.uint8)
+    
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    greatest_contour = max(contours, key=cv2.contourArea)
+    points = [Point(x=something[0][0], y=something[0][1]) for something in greatest_contour]
 
-    if history.annotation:
-        array_points = [[p.x, p.y] for p in history.annotation.shape.points]
-
-    inside_points = []
-    for p in history.inside_clicks:
-        inside_points.append([p.x, p.y])
-
-    outside_points = []
-    for p in history.outside_clicks:
-        outside_points.append([p.x, p.y])
-
-    bbox_points = outside_points
+    return points
 
     bbox = bounding_box(bbox_points)
     box_size = 100
