f82d388ce0a89bab1b870701de3679701b0ae96b
Philipp Glahe <philipp@zauberzeug.com>
2021-11-25 17:03:04 +0100

    annotation node: Gather foreground and background points.

--- demo_segmentation_tool/demo_segmentation_tool.py
+++ demo_segmentation_tool/demo_segmentation_tool.py
@@ -19,16 +19,36 @@ class Box(BaseModel):
 class History(BaseModel):
     bbox: Optional[Box]
     rect_creation_finished: bool = False
+    mouse_is_down: bool = False
     bg_pixel: List[Point] = []
     fg_pixel: List[Point] = []
+    path_pixels: List[Point] = []
     annotation: Optional[SegmentationAnnotation]
 
-    def to_svg(self) -> str:
+    def to_svg_rect(self) -> str:
 
         # width = abs(self.x2-self.x)
         # height = abs(self.y2-self.y)
         # return f'<rect x="{min(self.x, self.x2)}" y="{min(self.y, self.y2)}" width="{width or 1}" height="{height or 1}" fill="blue">'
-        return f'<rect x="{self.bbox.x}" y="{self.bbox.y}" width="{self.bbox.w}" height="{self.bbox.h}" stroke="blue" fill="transparent">'
+        return f'<rect x="{self.bbox.x}" y="{self.bbox.y}" width="{abs(self.bbox.w)}" height="{abs(self.bbox.h)}" stroke="blue" fill="transparent">'
+
+    def to_svg_path(self, shift_pressed: bool) -> str:
+        def create_path(pixel: List[Point]) -> str:
+            path = f'M {pixel[0].x} {pixel[0].y}'
+            for point in pixel[1:]:
+                path += f' L {point.x} {point.y}'
+            path += ' Z'
+            return path
+
+        svg_path = ''
+        if self.path_pixels:
+            if shift_pressed:
+                path = create_path(self.path_pixels)
+                svg_path = f'<path d="{path}" stroke="white" stroke-width="3" fill="white">'
+            else:
+                path = create_path(self.path_pixels)
+                svg_path = f'<path d="{path}" stroke="red" stroke-width="3" fill="red">'
+        return svg_path
 
 
 class DemoSegmentationTool(AnnotationTool):
@@ -36,7 +56,8 @@ class DemoSegmentationTool(AnnotationTool):
 
     async def handle_user_input(self, user_input: UserInput) -> ToolOutput:
         try:
-            if self.history.rect_creation_finished == True:
+            ic(self.history.rect_creation_finished)
+            if self.history.rect_creation_finished == True and user_input.data.event_type == EventType.Enter_Pressed:
                 self.history = None  # // simple resetting.
         except:
             pass
@@ -47,16 +68,15 @@ class DemoSegmentationTool(AnnotationTool):
             self.history = History()
             self.history.bbox = Box(x=coordinate.x, y=coordinate.y, w=0, h=0)
             # start bbox Creation
-            output.svg = self.history.to_svg()
+            output.svg = self.history.to_svg_rect()
             return output
 
         elif self.history.rect_creation_finished == False and user_input.data.event_type == EventType.MouseMove:
             # update update bbox
             self.history.bbox.w = coordinate.x - self.history.bbox.x
             self.history.bbox.h = coordinate.y - self.history.bbox.y
-            output.svg = self.history.to_svg()
+            output.svg = self.history.to_svg_rect()
             return output
-            pass
         elif self.history.rect_creation_finished == False and user_input.data.event_type == EventType.MouseUp:
             # end update bbox
             ic('hier')
@@ -64,26 +84,50 @@ class DemoSegmentationTool(AnnotationTool):
             self.history.bbox.h = coordinate.y - self.history.bbox.y
             self.history.rect_creation_finished = True
 
-            points = autofit(f'/data/{user_input.data.context.organization}/{user_input.data.context.project}/images/{user_input.data.image_uuid}.jpg',
-                             self.history)
+            try:
+                points = autofit(f'/data/{user_input.data.context.organization}/{user_input.data.context.project}/images/{user_input.data.image_uuid}.jpg',
+                                 self.history)
+            except:
+                self.history = None
             self.history.annotation = SegmentationAnnotation(id=str(uuid4()), shape=Shape(points=[]),
                                                              image_id=user_input.data.image_uuid, category_id=user_input.data.category.id)
             self.history.annotation.shape.points = points
             ic(points)
-            output.svg = self.history.to_svg()
+            output.svg = self.history.to_svg_rect()
             output.annotation = self.history.annotation
             return output
 
         elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseDown:
+            self.history.mouse_is_down = True
             # start gathering bg vg points
-            pass
-        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseMove:
+            self.history.fg_pixel.append(
+                user_input.data.coordinate) if user_input.data.is_shift_key_pressed else self.history.bg_pixel.append(user_input.data.coordinate)
+
+        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseMove and self.history.mouse_is_down:
             # gathering bg vg points
-            pass
+            self.history.fg_pixel.append(
+                user_input.data.coordinate) if user_input.data.is_shift_key_pressed else self.history.bg_pixel.append(user_input.data.coordinate)
+            ic(self.history.fg_pixel)
+            self.history.path_pixels.append(user_input.data.coordinate)
+            output.svg = self.history.to_svg_path(user_input.data.is_shift_key_pressed)
+            return output
+
+        elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseMove:
+            return output
+
         elif self.history.rect_creation_finished == True and user_input.data.event_type == EventType.MouseUp:
+            # editing complete
+            self.history.mouse_is_down = False
+            self.history.path_pixels = []
             # start gathering bg vg points
+            self.history.fg_pixel.append(
+                user_input.data.coordinate) if user_input.data.is_shift_key_pressed else self.history.bg_pixel.append(user_input.data.coordinate)
             # crabcut
-            pass
+            points = autofit(f'/data/{user_input.data.context.organization}/{user_input.data.context.project}/images/{user_input.data.image_uuid}.jpg',
+                             self.history)
+            self.history.annotation.shape.points = points
+            output.annotation = self.history.annotation
+            return output
         else:
             import logging
             logging.error('Invalid State')
@@ -138,13 +182,20 @@ def autofit(image_path, history: History) -> List[Point]:
          crop_rect[0]:crop_rect[0]+crop_rect[2]] = cv2.GC_PR_FGD
     mask[crop_rect[1]+crop_rect[3]//4:crop_rect[1]+3*crop_rect[3]//4,
          crop_rect[0]+crop_rect[2]//4:crop_rect[0]+3*crop_rect[2]//4] = cv2.GC_FGD
+
+    for p in history.fg_pixel:
+        mask[p.y, p.x] = cv2.GC_FGD
+
+    for p in history.bg_pixel:
+        mask[p.y, p.x] = cv2.GC_BGD
+
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
     cv2.grabCut(img, mask, crop_rect, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK)
     # rows, cols = np.where(mask % 2 > 0)
 
     mask = np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 0, 1).astype(np.uint8)
-    
+
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     greatest_contour = max(contours, key=cv2.contourArea)
     points = [Point(x=something[0][0], y=something[0][1]) for something in greatest_contour]
