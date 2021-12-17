32653b384c45252527741397c8db64c9bc43d95e
Maik Switalski <maik@zauberzeug.com>
2021-12-17 09:21:33 +0100

    converter: create own test data to be independent from implicit used demo project

--- learning_loop_node/converter/tests/test_converter.py
+++ learning_loop_node/converter/tests/test_converter.py
@@ -5,6 +5,7 @@ import logging
 from learning_loop_node.model_information import ModelInformation
 from learning_loop_node.converter.converter import Converter
 from learning_loop_node.converter.converter_node import ConverterNode
+from learning_loop_node.tests import test_helper
 
 
 class MockedConverter(Converter):
@@ -14,13 +15,28 @@ class MockedConverter(Converter):
         self.models.append(model_information)
 
 
+@pytest.fixture()
+def create_project():
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+    project_configuration = {'project_name': 'pytest', 'box_categories': 1,  'point_categories': 1, 'inbox': 0, 'annotate': 0, 'review': 0, 'complete': 0, 'image_style': 'plain',
+                             'thumbs': False}
+    assert test_helper.LiveServerSession().post(f"/api/zauberzeug/projects/generator",
+                                                json=project_configuration).status_code == 200
+    yield
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+
+
 @pytest.mark.asyncio
-async def test_meta_informations():
+async def test_meta_informations(create_project):
+    model_id = await test_helper.assert_upload_model()
+
     converter = MockedConverter(source_format='mocked', target_format='test')
     node = ConverterNode(name='test', converter=converter)
     await node.convert_models()
-    assert len(converter.models) == 3
-    categories = converter.models[0].categories
-    assert len(categories) == 6
+
+    pytest_project_model = [m for m in converter.models if m.id == model_id][0]
+
+    categories = pytest_project_model.categories
+    assert len(categories) == 2
     assert categories[0].type == 'box'
     assert categories[1].type == 'point'
