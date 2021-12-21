b3bd77344b84a3891cc7ca9acd167e62361a768f
Maik Switalski <maik@zauberzeug.com>
2021-12-21 16:48:19 +0100

    detector: introduce detector. Implement rest api /detect

--- learning_loop_node/detector/detector_node.py
+++ learning_loop_node/detector/detector_node.py
@@ -7,73 +7,109 @@ from learning_loop_node.context import Context
 from learning_loop_node.rest import downloads
 from fastapi.encoders import jsonable_encoder
 from fastapi_utils.tasks import repeat_every
-from typing import Union
+from typing import List, Optional
 import shutil
 import os
 import logging
 from learning_loop_node.globals import GLOBALS
 from icecream import ic
-import json
 import subprocess
+from detector.detector import Detector
+import asyncio
+import cv2
+from learning_loop_node.detector.rest import detect
+from learning_loop_node.detector.rest import upload
+from learning_loop_node.detector.detections import Detections
 
 
 class DetectorNode(Node):
-    current_model_id: Union[str, None] = None
-    target_model_id: Union[str, None] = None
+    detector: Detector
     organization: str
     project: str
     operation_mode: OperationMode = OperationMode.Idle
 
-    def __init__(self, name: str, uuid: str = None):
-        super().__init__(name, uuid)
+    target_model_id: Optional[str]
 
+    def __init__(self, name: str, detector: Detector, uuid: str = None):
+        super().__init__(name, uuid)
+        self.detector = detector
         self.organization = os.environ.get('LOOP_ORGANIZATION', None) or os.environ.get('ORGANIZATION', None)
         self.project = os.environ.get('LOOP_PROJECT', None) or os.environ.get('PROJECT', None)
         assert self.organization, 'Detector node needs an organization'
         assert self.project, 'Detector node needs an project'
 
+        self.include_router(detect.router, tags=["detect"])
         self.include_router(operation_mode.router, prefix="")
+        self.include_router(operation_mode.router, tags=["operation_mode"])
 
         @self.on_event("startup")
-        @repeat_every(seconds=1, raise_exceptions=False, wait_first=False)
+        @repeat_every(seconds=3, raise_exceptions=False, wait_first=False)
         async def _check_for_update() -> None:
             await self.check_for_update()
 
-        try:
-            with open(f'{GLOBALS.data_folder}/model/model.json', 'r') as f:
-                content = json.load(f)
-                self.current_model_id = content['id']
-        except:
-            pass
+        @self.on_event("startup")
+        async def _load_model() -> None:
+            self.detector.load_model()
+            await self.check_for_update()
 
     async def check_for_update(self):
-        if self.operation_mode == OperationMode.Check_for_updates:
-            logging.info('going to check for new updates')
-            await self.send_status()
-            if self.target_model_id != self.current_model_id:
-                target_model_folder = f'{GLOBALS.data_folder}/models/{self.target_model_id}'
-                shutil.rmtree(target_model_folder, ignore_errors=True)
-                os.makedirs(target_model_folder)
-                await downloads.download_model(target_model_folder, Context(organization=self.organization, project=self.project), self.target_model_id, 'mocked')
-                shutil.rmtree(f'{GLOBALS.data_folder}/model', ignore_errors=True)
-                shutil.copytree(target_model_folder, f'{GLOBALS.data_folder}/model')
+        try:
+            logging.info(f'periodically checking operation mode. Currently the mode is {self.operation_mode}')
+            if self.detector.current_model:
+                logging.info(
+                    f'Current model : { self.detector.current_model.version} with id { self.detector.current_model.id}')
+            else:
+                logging.info(f'Current model is None')
+            if self.operation_mode == OperationMode.Check_for_updates:
+                logging.info('going to check for new updates')
+                await self.send_status()
+                logging.info('Check if the current model id matches the target_model_id')
 
-                self.reload()
+                if not self.detector.current_model or self.target_model_id != self.detector.current_model.id:
+                    logging.info('No match. Going to download new version')
+                    model_symlink = f'{GLOBALS.data_folder}/model'
+                    target_model_folder = f'{GLOBALS.data_folder}/models/{self.target_model_id}'
+                    shutil.rmtree(target_model_folder, ignore_errors=True)
+                    os.makedirs(target_model_folder)
+                    await downloads.download_model(target_model_folder, Context(organization=self.organization, project=self.project), self.target_model_id, self.detector.model_format)
+                    try:
+                        os.unlink(model_symlink)
+                        os.remove(model_symlink)
+                    except:
+                        pass
+                    os.symlink(target_model_folder, model_symlink)
+                    logging.info(f'Updated symlink for model to {os.readlink(model_symlink)}')
+                    self.reload()
+                else:
+                    logging.info('Versions are identic. Nothing to do.')
+            else:
+                logging.info('###### Operation mode was NOT check_for_updates')
+        except Exception as e:
+            logging.error(f'An error occured during "check_for_update". {str(e)}')
+            raise
 
     async def send_status(self) -> dict:
+        current_model_id = None
+        try:
+            current_model_id = self.detector.current_model.id
+        except:
+            pass
         status = DetectionStatus(
             id=self.uuid,
             name=self.name,
             state=self.status.state,
             operation_mode=self.operation_mode,
-            current_model_id=self.current_model_id,
-            target_model_id=self.target_model_id,
+            current_model_id=current_model_id,
             latest_error=self.status.latest_error,
         )
-
         logging.info(f'sending status {status}')
         response = await self.sio_client.call('update_detector', (self.organization, self.project, jsonable_encoder(status)), timeout=1)
-        self.target_model_id = response['payload']['target_model_id']
+        try:
+            self.target_model_id = response['payload']['target_model_id']
+            logging.info(f'After sending status. Target_model_id is {self.target_model_id}')
+        except:
+            logging.error('Could not send status to loop')
+            return False
         return True
 
     def get_state(self):
@@ -85,3 +121,14 @@ class DetectorNode(Node):
 
     def reload(self):
         subprocess.call(["touch", "/app/restart/restart.py"])
+
+    async def get_detections(self, np_image, mac: str, tags: str, active_learning=True):
+        loop = asyncio.get_event_loop()
+        image = await loop.run_in_executor(None, lambda: cv2.imdecode(np_image, cv2.IMREAD_COLOR))
+        detections = await loop.run_in_executor(None, lambda: self.detector.evaluate(image))
+        info = "\n    ".join([str(d) for d in detections.box_detections])
+        logging.info(f'detected:\n    {info}')
+        # if active_learning:
+        #     thread = Thread(target=learn, args=(detections, mac, tags, image))
+        #     thread.start()
+        return jsonable_encoder(detections)
---
+++ learning_loop_node/detector/rest/detect.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Request, File, UploadFile, Header
+from typing import Optional
+import numpy as np
+from fastapi.responses import JSONResponse
+from icecream import ic
+
+
+router = APIRouter()
+
+
+@router.post("/detect")
+async def http_detect(request: Request, file: UploadFile = File(...), mac: str = Header(...), tags: Optional[str] = Header(None)):
+    """
+    Example Usage
+
+        curl --request POST -H 'mac: FF:FF' -F 'file=@test.jpg' localhost:8004/detect
+
+        for i in `seq 1 10`; do time curl --request POST -H 'mac: FF:FF' -F 'file=@test.jpg' localhost:8004/detect; done
+    """
+    try:
+        np_image = np.fromfile(file.file, np.uint8)
+    except:
+        raise Exception(f'Uploaded file {file.filename} is no image file.')
+    detections = await request.app.get_detections(np_image, mac, tags.split(',') if tags else [])
+
+    return JSONResponse(detections)
