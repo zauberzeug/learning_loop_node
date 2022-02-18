2da398ab1f4cf008275ac7455a4277a0387df95b
Maik Switalski <maik@zauberzeug.com>
2022-02-18 09:05:37 +0100

    trainer, calculate detections after training: remove ErrorConfiguration.stop_training. We do not know what to do in this case, yet.

--- learning_loop_node/trainer/error_configuration.py
+++ learning_loop_node/trainer/error_configuration.py
@@ -5,7 +5,5 @@ from pydantic.main import BaseModel
 class ErrorConfiguration(BaseModel):
     begin_training: Optional[bool] = False
     save_model: Optional[bool] = False
-    stop_training: Optional[bool] = False
-    stop_training: Optional[bool] = False
     get_new_model: Optional[bool] = False
     crash_training: Optional[bool] = False
