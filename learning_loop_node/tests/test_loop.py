7af917768e2c1a81a933cc043bad6aec1d4146ae
Maik Switalski <maik@zauberzeug.com>
2022-03-31 06:14:41 +0000

    fix test by downloading different model.

--- learning_loop_node/tests/test_loop.py
+++ learning_loop_node/tests/test_loop.py
@@ -7,15 +7,15 @@ import logging
 import os
 from icecream import ic
 
+
 @pytest.mark.asyncio
 async def test_multiple_get_requests_after_post_request_should_not_causes_timeout_error():
     logging.debug('downloading model from gdrive')
     if not os.path.exists('/tmp/some_model/model.pt'):
-        file_id = '1q8nT-CTHt1eZuNjPMbdaavyFnMtDRT-L'
+        file_id = '1sZWa053fWT9PodrujDX90psmjhFVLyBV'
         destination = '/tmp/model.zip'
         g_download(file_id, destination)
         test_helper.unzip(destination, '/tmp/some_model')
-
 
     data = test_helper.prepare_formdata(['/tmp/some_model/model.pt'])
     from learning_loop_node.loop import loop
@@ -29,4 +29,3 @@ async def test_multiple_get_requests_after_post_request_should_not_causes_timeou
     for i in range(10):
         ic('going to download model from loop')
         await downloads.download_model('tmp', Context(organization='zauberzeug', project='pytest'), model['id'], 'yolov5_pytorch')
-
