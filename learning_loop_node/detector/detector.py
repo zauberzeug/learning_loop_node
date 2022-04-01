0067ef12e2d3f5596d1601e84078d4ef87d7b5ed
Rodja Trappe <rodja@zauberzeug.com>
2022-04-01 15:00:46 +0000

    #295 made loading model.json from disk available to all nodes

--- learning_loop_node/detector/detector.py
+++ learning_loop_node/detector/detector.py
@@ -1,9 +1,6 @@
 from typing import Optional, Union, Any
-import os
-import json
 import logging
 from learning_loop_node.model_information import ModelInformation
-from learning_loop_node.globals import GLOBALS
 from learning_loop_node.detector.detections import Detections
 
 
@@ -16,7 +13,7 @@ class Detector():
 
     def load_model(self):
         try:
-            model_information = self.load_model_iformation()
+            model_information = ModelInformation.load_from_disk()
             try:
                 self.init(model_information)
                 self.current_model = model_information
@@ -24,29 +21,9 @@ class Detector():
             except Exception:
                 logging.error(f'Could not init model {model_information}')
                 raise
-
         except Exception:
             self.current_model = None
             logging.exception('An error occured during loading model.')
-
-    def load_model_iformation(self):
-        model_root_path = f'{GLOBALS.data_folder}/model'
-        model_info_file_path = f'{model_root_path}/model.json'
-        if not os.path.exists(model_info_file_path):
-            raise FileExistsError(f"File '{model_info_file_path}' does not exist.")
-        with open(model_info_file_path, 'r') as f:
-            try:
-                content = json.load(f)
-            except:
-                raise Exception(f"could not read model information from file '{model_info_file_path}'")
-            try:
-                model_information = ModelInformation.parse_obj(content)
-                model_information.model_root_path = model_root_path
-            except Exception as e:
-                raise Exception(
-                    f"could not parse model information from file '{model_info_file_path}'. \n {str(e)}")
-
-        return model_root_path, model_information
 
     def init(self,  model_info: ModelInformation, model_root_path: str):
         raise NotImplementedError()
--- learning_loop_node/model_information.py
+++ learning_loop_node/model_information.py
@@ -2,6 +2,9 @@ from pydantic.main import BaseModel
 from typing import List, Optional
 from learning_loop_node.context import Context
 from learning_loop_node.data_classes.category import Category
+from learning_loop_node.globals import GLOBALS
+import os
+import json
 
 
 class ModelInformation(BaseModel):
@@ -17,3 +20,23 @@ class ModelInformation(BaseModel):
     @property
     def context(self):
         return Context(organization=self.organization, project=self.project)
+
+    @staticmethod
+    def load_from_disk():
+        model_root_path = f'{GLOBALS.data_folder}/model'
+        model_info_file_path = f'{model_root_path}/model.json'
+        if not os.path.exists(model_info_file_path):
+            raise FileExistsError(f"File '{model_info_file_path}' does not exist.")
+        with open(model_info_file_path, 'r') as f:
+            try:
+                content = json.load(f)
+            except:
+                raise Exception(f"could not read model information from file '{model_info_file_path}'")
+            try:
+                model_information = ModelInformation.parse_obj(content)
+                model_information.model_root_path = model_root_path
+            except Exception as e:
+                raise Exception(
+                    f"could not parse model information from file '{model_info_file_path}'. \n {str(e)}")
+
+        return model_information
