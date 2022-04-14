a002cec8f5943ec55ed15c3815396779bf3f3495
Rodja Trappe <rodja@zauberzeug.com>
2022-04-14 15:45:10 +0200

    zucker-99: consolidated tests

--- learning_loop_node/detector/tests/test_relevance_filter.py
+++ learning_loop_node/detector/tests/test_relevance_filter.py
@@ -6,30 +6,16 @@ import asyncio
 
 
 @pytest.mark.asyncio
-async def test_default_relevance_filter_is_used_by_node(test_detector_node: DetectorNode):
+@pytest.mark.parametrize('autoupload,expected_file_count', [(None, 2), ('all', 4)])
+async def test_filter_is_used_by_node(test_detector_node: DetectorNode, autoupload, expected_file_count):
     assert test_detector_node.outbox.path.startswith('/tmp')
     assert len(get_outbox_files(test_detector_node.outbox)) == 0
 
     image = np.fromfile('detector/tests/test.jpg', np.uint8)
-    _ = await test_detector_node.get_detections(image, '00:.....', tags=[])
+    _ = await test_detector_node.get_detections(image, '00:.....', tags=[], autoupload=autoupload)
     # NOTE adding second images with identical detections
-    _ = await test_detector_node.get_detections(image, '00:.....', tags=[])
+    _ = await test_detector_node.get_detections(image, '00:.....', tags=[], autoupload=autoupload)
     await asyncio.sleep(1)  # files are stored asynchronously
 
-    assert len(get_outbox_files(test_detector_node.outbox)) == 2,\
-        'There should be 1 image and 1 .json file in the outbox'
-
-
-@pytest.mark.asyncio
-async def test_node_can_be_queried_without_any_filter(test_detector_node: DetectorNode):
-    assert test_detector_node.outbox.path.startswith('/tmp')
-    assert len(get_outbox_files(test_detector_node.outbox)) == 0
-
-    image = np.fromfile('detector/tests/test.jpg', np.uint8)
-    _ = await test_detector_node.get_detections(image, '00:.....', tags=[], autoupload='all')
-    # NOTE adding second images with identical detections
-    _ = await test_detector_node.get_detections(image, '00:.....', tags=[], autoupload='all')
-    await asyncio.sleep(1)  # files are stored asynchronously
-
-    assert len(get_outbox_files(test_detector_node.outbox)) == 4,\
-        'there should be 2 images and 2 .json file in the outbox'
+    assert len(get_outbox_files(test_detector_node.outbox)) == expected_file_count,\
+        'There should be 1 image and 1 .json file for every detection in the outbox'
