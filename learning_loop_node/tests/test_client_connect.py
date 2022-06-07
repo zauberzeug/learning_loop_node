f65e1e206c81eb9db7572dbe6c0ba922436be88e
Philipp Glahe <philipp@zauberzeug.com>
2022-06-07 08:15:43 +0000

    #507 loop: Fix test

--- learning_loop_node/tests/test_client_connect.py
+++ learning_loop_node/tests/test_client_connect.py
@@ -5,6 +5,6 @@ from learning_loop_node.loop import loop
 @pytest.mark.asyncio
 async def test_connect_headers():
     headers = loop.get_headers()
-    assert headers['node_type'] == 'trainer'
+    assert 'node_type' in headers
     assert 'organization' in headers
     assert 'project' in headers
