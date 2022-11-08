1a81f4b547eaccf0981afafa4bc2e6cddf704308
Maik Switalski <maik@zauberzeug.com>
2022-11-08 11:05:29 +0100

    #656 trainer, state: provide pretrained models for testing trainer.

--- learning_loop_node/trainer/tests/testing_trainer.py
+++ learning_loop_node/trainer/tests/testing_trainer.py
@@ -1,4 +1,6 @@
+from typing import List
 from learning_loop_node.trainer import Trainer
+from learning_loop_node.trainer.model import PretrainedModel
 
 
 class TestingTrainer(Trainer):
@@ -9,3 +11,10 @@ class TestingTrainer(Trainer):
 
     async def start_training(self) -> None:
         self.executor.start('while true; do sleep 1; done')
+
+    @property
+    def provided_pretrained_models(self) -> List[PretrainedModel]:
+        return [
+            PretrainedModel(name='small', label='Small', description='a small model'),
+            PretrainedModel(name='medium', label='Medium', description='a medium model'),
+            PretrainedModel(name='large', label='Large', description='a large model')]
