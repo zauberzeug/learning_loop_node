ce8eed839844b276944c1f60335869a2c766ec9a
Maik Switalski <maik@zauberzeug.com>
2022-11-18 09:26:06 +0100

    #656 trainer, state: fix test for successfully uploading detections

--- learning_loop_node/trainer/tests/states/test_upload_detections_state.py
+++ learning_loop_node/trainer/tests/states/test_upload_detections_state.py
@@ -1,3 +1,4 @@
+from learning_loop_node.detector.box_detection import BoxDetection
 from learning_loop_node.trainer.tests.testing_trainer import TestingTrainer
 from learning_loop_node.trainer.tests.states import state_helper
 from learning_loop_node.trainer.tests.states.state_helper import assert_training_state
@@ -6,7 +7,8 @@ from learning_loop_node.trainer.trainer import Trainer
 from learning_loop_node.trainer.training import Training
 from learning_loop_node.context import Context
 import asyncio
-
+from learning_loop_node import loop
+from fastapi.encoders import jsonable_encoder
 error_key = 'upload_detections'
 
 
@@ -14,18 +16,29 @@ def trainer_has_error(trainer: Trainer):
     return trainer.errors.has_error_for(error_key)
 
 
-def create_detection_file(training: Training):
-    active_training.save_detections(training, [{'some_bad_data': 'some_bad_data'}])
+async def create_valid_detection_file(training: Training):
+    async with loop.get(f'/api/zauberzeug/projects/demo/data') as response:
+        assert response.status == 200, response
+        content = await response.json()
+
+    category = content['categories'][0]
+    image_id = content['image_ids'][0]
+    model_version = '1.2'
+    box_detections = [BoxDetection(category['name'], x=1, y=2, width=30, height=40,
+                                   net=model_version, confidence=.99, category_id=category['id'])]
+
+    image_entry = {'image_id': image_id, 'box_detections': box_detections,
+                   'point_detections': [], 'segmentation_detections': []}
+    active_training.save_detections(training, jsonable_encoder([image_entry]))
 
 
 async def test_upload_successfull():
-    # TODO How can we create detections for uploading?
-    # Or should we mock it?
-    return
+
     state_helper.create_active_training_file(training_state='detected')
     trainer = TestingTrainer()
     trainer.training = active_training.load()  # normally done by node
-    await create_detection_file(trainer.training)
+
+    await create_valid_detection_file(trainer.training)
 
     await trainer.upload_detections()
 
@@ -38,7 +51,7 @@ async def test_bad_status_from_LearningLoop():
         organization='zauberzeug', project='some_bad_project'))
     trainer = TestingTrainer()
     trainer.training = active_training.load()  # normally done by node
-    create_detection_file(trainer.training)
+    active_training.save_detections(trainer.training, [{'some_bad_data': 'some_bad_data'}])
 
     train_task = asyncio.get_running_loop().create_task(trainer.train(None, None))
     await assert_training_state(trainer.training, 'detection_uploading', timeout=1, interval=0.001)
