dfd2845b38cea8a237ba531900488c74dd90a224
Philipp Glahe <philipp@zauberzeug.com>
2023-02-17 13:22:01 +0100

    #817 trainer: Upload detections in smaller batches

--- learning_loop_node/trainer/trainer.py
+++ learning_loop_node/trainer/trainer.py
@@ -374,7 +374,7 @@ class Trainer():
 
     async def _upload_detections(self, context: Context, detections: List[dict]):
         logging.info('uploading detections')
-        batch_size = 500
+        batch_size = 50
 
         skip_detections = active_training.detections_upload_progress.load(self.training)
 
