00217bf93ea6a17e48091019ce2eb5288b575c15
Dennis Wittich <dennis@zauberzeug.com>
2023-09-21 15:01:54 +0200

    add debug print

--- demo_segmentation_tool/main.py
+++ demo_segmentation_tool/main.py
@@ -10,5 +10,6 @@ tool = AnnotatorLogic()
 node = AnnotatorNode(name=f'Demo tool  {os.uname()[1]}',
                      uuid='00000000-1111-2222-3333-555555555555', annotator_logic=tool)
 
+print(f"Starting node {node.name} with id {node.uuid}")
 if __name__ == "__main__":
     uvicorn.run("main:node", host="0.0.0.0", port=80, lifespan='on', use_colors=True, reload=True)
