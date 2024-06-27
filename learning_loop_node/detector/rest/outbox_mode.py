8b0516dd8ecb9abe63784080b651aacdb3c24800
Niklas Neugebauer <niklas@zauberzeug.com>
2024-06-27 10:32:02 +0200

    match returned responses of other endpoints

--- learning_loop_node/detector/rest/outbox_mode.py
+++ learning_loop_node/detector/rest/outbox_mode.py
@@ -1,21 +1,35 @@
 
-from fastapi import APIRouter, HTTPException, Response
+import logging
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.responses import PlainTextResponse
+
 
 router = APIRouter()
 
 
 @router.get("/outbox_mode")
-async def get_outbox_mode(request):
+async def get_outbox_mode(request: Request):
+    '''
+    Example Usage
+        curl http://localhost/outbox_mode
+    '''
+    logging.error("\nget_outbox_mode\n")
     outbox = request.app.outbox
-    return Response(content=str(outbox.get_mode()))
+    return PlainTextResponse(str(outbox.get_mode()))
 
 
 @router.put("/outbox_mode")
-async def put_outbox_mode(request):
+async def put_outbox_mode(request: Request):
+    '''
+    Example Usage
+        curl -X PUT -d "continuous_upload" http://localhost/outbox_mode
+        curl -X PUT -d "stopped" http://localhost/outbox_mode
+    '''
+    print("put_outbox_mode")
     outbox = request.app.outbox
     content = str(await request.body(), 'utf-8')
     try:
         outbox.set_mode(content)
     except ValueError as e:
         raise HTTPException(422, str(e)) from e
-    return Response(status_code=200)
+    return "OK"
