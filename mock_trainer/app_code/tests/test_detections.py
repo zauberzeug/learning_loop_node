33f23817286f11584bb9027bba16a4f265f8adac
Dr. Dennis Wittich <denniswittich@hotmail.de>
2024-07-05 12:25:58 +0200

    fix mock_trainer tests

--- mock_trainer/app_code/tests/test_detections.py
+++ mock_trainer/app_code/tests/test_detections.py
@@ -19,17 +19,20 @@ async def test_all(setup_test_project1, glc: LoopCommunicator):
     assert_image_count(0)
     assert GLOBALS.data_folder == '/tmp/learning_loop_lib_data'
 
-    latest_model_id = await test_helper.get_latest_model_id(project='pytest_p1')
+    latest_model_id = await test_helper.get_latest_model_id(project='pytest_mock_trainer_test1')
 
     trainer = MockTrainerLogic(model_format='mocked')
     node = TrainerNode(name='test', trainer_logic=trainer)
-    context = Context(organization='zauberzeug', project='pytest_p1')
+    context = Context(organization='zauberzeug', project='pytest_mock_trainer_test1')
     details = {'categories': [jsonable_encoder(asdict(Category(id='some_id', name='some_category_name')))],
-               'id': '917d5c7f-403d-7e92-f95f-577f79c2273a',  # version 1.2 of demo project
+               'id': '798bfbf1-8948-2ea9-32fb-3571b6748bca',  # version 1.2 of demo project
                'training_number': 0,
                'resolution': 800,
                'flip_rl': False,
                'flip_ud': False}
+
+    # await asyncio.sleep(100)
+
     trainer._node = node
     trainer._init_new_training(context=context, details=details)
 
@@ -49,7 +52,7 @@ async def test_all(setup_test_project1, glc: LoopCommunicator):
 
 
 def assert_image_count(value: int):
-    images_folder = f'{GLOBALS.data_folder}/zauberzeug/pytest_p1'
+    images_folder = f'{GLOBALS.data_folder}/zauberzeug/pytest_mock_trainer_test1'
     files = glob(f'{images_folder}/**/*.*', recursive=True)
     files = [file for file in files if file.endswith('.jpg')]
     assert len(files) == value
