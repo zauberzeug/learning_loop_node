d7df7c64e5d1bcae2e8084b840f30107f2d615f4
Dr. Dennis Wittich <denniswittich@hotmail.de>
2024-10-24 14:22:39 +0200

    wait longer for TrainerState change

--- learning_loop_node/tests/trainer/states/test_state_download_train_model.py
+++ learning_loop_node/tests/trainer/states/test_state_download_train_model.py
@@ -55,7 +55,7 @@ async def test_downloading_failed(test_initialized_trainer: TestingTrainerLogic)
 
     trainer._begin_training_task()
     await assert_training_state(trainer.training, TrainerState.TrainModelDownloading, timeout=1, interval=0.001)
-    await assert_training_state(trainer.training, TrainerState.DataDownloaded, timeout=1, interval=0.001)
+    await assert_training_state(trainer.training, TrainerState.DataDownloaded, timeout=5, interval=0.001)
 
     assert trainer.errors.has_error_for('download_model')
     assert trainer._training is not None  # pylint: disable=protected-access
