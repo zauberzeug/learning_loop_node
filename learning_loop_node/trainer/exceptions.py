ca1aab527930e9e3d9b43d0ef32ee3595399f49d
Dr. Dennis Wittich <denniswittich@hotmail.de>
2024-10-25 09:56:05 +0200

    feature: trainer logic (#37)
    adds a exception type that triggers a node restart

--- learning_loop_node/trainer/exceptions.py
+++ learning_loop_node/trainer/exceptions.py
@@ -1,2 +1,12 @@
 class CriticalError(Exception):
-    pass
+    '''
+    CriticalError is raised when the training cannot be continued.
+    In this case the trainer jumps to the TrainerState.ReadyForCleanup and tries to upload the latest model.
+    '''
+
+
+class NodeNeedsRestartError(Exception):
+    '''
+    NodeNeedsRestartError is raised when the node needs to be restarted.
+    This is e.g. the case when the GPU is not available anymore.
+    '''
