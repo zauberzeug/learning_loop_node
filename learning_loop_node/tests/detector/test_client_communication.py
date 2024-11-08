650df821188d9f9e84123279c8456b4c8331f136
Dr. Dennis Wittich <denniswittich@hotmail.de>
2024-11-08 15:44:26 +0100

    try reduce test flakyness

--- learning_loop_node/tests/detector/test_client_communication.py
+++ learning_loop_node/tests/detector/test_client_communication.py
@@ -93,7 +93,7 @@ async def test_sio_upload(test_detector_node: DetectorNode, sio_client):
 
 # NOTE: This test seems to be flaky.
 async def test_about_endpoint(test_detector_node: DetectorNode):
-    await asyncio.sleep(11)
+    await asyncio.sleep(16)
     response = requests.get(f'http://localhost:{GLOBALS.detector_port}/about', timeout=30)
 
     assert response.status_code == 200, response.content
@@ -108,7 +108,7 @@ async def test_about_endpoint(test_detector_node: DetectorNode):
 
 
 async def test_model_version_api(test_detector_node: DetectorNode):
-    await asyncio.sleep(11)
+    await asyncio.sleep(16)
 
     response = requests.get(f'http://localhost:{GLOBALS.detector_port}/model_version', timeout=30)
     assert response.status_code == 200, response.content
