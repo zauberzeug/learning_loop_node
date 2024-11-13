39c4dd9385df6b130f2ea88476fe0f314f3cfa77
Niklas Neugebauer <niklas@zauberzeug.com>
2024-11-13 14:11:59 +0100

    move None check back outside _is_valid_isoformat

--- learning_loop_node/detector/outbox.py
+++ learning_loop_node/detector/outbox.py
@@ -76,7 +76,7 @@ class Outbox():
             return
         tmp = f'{GLOBALS.data_folder}/tmp/{identifier}'
         image_metadata.tags = tags
-        if self._is_valid_isoformat(creation_date):
+        if creation_date is None or self._is_valid_isoformat(creation_date):
             image_metadata.created = creation_date
         else:
             image_metadata.created = identifier
@@ -95,9 +95,7 @@ class Outbox():
         else:
             self.log.error('Could not rename %s to %s', tmp, self.path + '/' + identifier)
 
-    def _is_valid_isoformat(self, date: Optional[str]) -> bool:
-        if date is None:
-            return True
+    def _is_valid_isoformat(self, date: str) -> bool:
         try:
             datetime.fromisoformat(date)
             return True
