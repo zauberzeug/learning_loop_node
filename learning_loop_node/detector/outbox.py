c892bc951c67d25722122b6c449e3d1a499de8cc
Niklas Neugebauer <niklas@zauberzeug.com>
2025-01-24 10:58:13 +0100

    revert: change timeout back to float

--- learning_loop_node/detector/outbox.py
+++ learning_loop_node/detector/outbox.py
@@ -148,7 +148,7 @@ class Outbox():
 
         try:
             async with aiohttp.ClientSession() as session:
-                response = await session.post(self.target_uri, data=data, timeout=aiohttp.ClientTimeout(total=self.UPLOAD_TIMEOUT_S))
+                response = await session.post(self.target_uri, data=data, timeout=self.UPLOAD_TIMEOUT_S)
         except Exception:
             self.log.exception('Could not upload images')
             return
