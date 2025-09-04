92b5e1f03c26cfe59dcf3dc1edc2065515aa8ec2
Dr. Dennis Wittich <dennis@zauberzeug.com>
2025-09-04 13:13:41 +0200

    format/rename

--- learning_loop_node/data_classes/__init__.py
+++ learning_loop_node/data_classes/__init__.py
@@ -1,17 +1,43 @@
 from .annotations import AnnotationData, SegmentationAnnotation, ToolOutput, UserInput
-from .detections import (BoxDetection, ClassificationDetection, Detections, Observation, Point, PointDetection,
-                         SegmentationDetection, Shape)
-from .general import (AboutResponse, AnnotationNodeStatus, Category, Context, DetectionStatus, ErrorConfiguration,
-                      ModelInformation, ModelVersionResponse, NodeState, NodeStatus)
+from .detections import (
+    BoxDetection,
+    ClassificationDetection,
+    Detections,
+    Observation,
+    Point,
+    PointDetection,
+    SegmentationDetection,
+    Shape,
+)
+from .general import (
+    AboutResponse,
+    AnnotationNodeStatus,
+    Category,
+    Context,
+    DetectorStatus,
+    ErrorConfiguration,
+    ModelInformation,
+    ModelVersionResponse,
+    NodeState,
+    NodeStatus,
+)
 from .image_metadata import ImageMetadata, ImagesMetadata
 from .socket_response import SocketResponse
-from .training import Errors, PretrainedModel, Training, TrainingError, TrainingOut, TrainingStateData, TrainingStatus
+from .training import (
+    Errors,
+    PretrainedModel,
+    Training,
+    TrainingError,
+    TrainingOut,
+    TrainingStateData,
+    TrainingStatus,
+)
 
 __all__ = [
     'AboutResponse', 'AnnotationData', 'SegmentationAnnotation', 'ToolOutput', 'UserInput',
     'BoxDetection', 'ClassificationDetection', 'ImageMetadata', 'Observation', 'Point', 'PointDetection',
     'SegmentationDetection', 'Shape', 'Detections',
-    'AnnotationNodeStatus', 'Category', 'Context', 'DetectionStatus', 'ErrorConfiguration',
+    'AnnotationNodeStatus', 'Category', 'Context', 'DetectorStatus', 'ErrorConfiguration',
     'ModelInformation', 'NodeState', 'NodeStatus', 'ModelVersionResponse', 'ImagesMetadata',
     'SocketResponse',
     'Errors', 'PretrainedModel', 'Training',
