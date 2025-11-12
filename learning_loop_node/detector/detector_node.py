9ef8ced678acac6acc1447d1ef793a372eb66edb
Niklas Neugebauer <niklas@zauberzeug.com>
2025-11-12 12:48:52 +0100

    feat: increase socketio max_http_buffer_size to 60M bytes

--- learning_loop_node/detector/detector_node.py
+++ learning_loop_node/detector/detector_node.py
@@ -214,10 +214,10 @@ class DetectorNode(Node):
         """The DetectorNode acts as a SocketIO server. This method sets up the server and defines the event handlers."""
         # pylint: disable=unused-argument
 
-        # Initialize the Socket.IO server with 20MB buffer size
+        # Initialize the Socket.IO server with 60MB buffer size
         self.sio = socketio.AsyncServer(
             async_mode='asgi',
-            max_http_buffer_size=2e7,  # 20MB
+            max_http_buffer_size=6e7,  # 60MB
         )
         # Initialize and mount the ASGI app
         self.sio_app = socketio.ASGIApp(self.sio, socketio_path='/socket.io')
