5aa49f91ba7877420f0180e0a246f9373ba08c21
ftilde <dominik@zauberzeug.com>
2026-02-25 10:39:28 +0100

    Allow data folder to be specified via env var (#77)
    - `DATA_FOLDER` env var now overwrites `/data` default location
    - Update project version to (likely) next released version

--- learning_loop_node/globals.py
+++ learning_loop_node/globals.py
@@ -1,7 +1,9 @@
+import os
+
 
 class Globals():
     def __init__(self) -> None:
-        self.data_folder: str = '/data'
+        self.data_folder: str = os.getenv('DATA_FOLDER', '/data')
         self.detector_port: int = 5004  # NOTE used for tests
 
 
