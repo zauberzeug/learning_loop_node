88e420222b3948aa3c3caa3cd9f943ef3d227746
Dennis <dennis@zauberzeug.com>
2026-04-01 13:30:29 +0200

    improve style of Mock implementations

--- mock_detector/app_code/mock_detector.py
+++ mock_detector/app_code/mock_detector.py
@@ -1,11 +1,16 @@
-from typing import List
+from typing import final
 
 import numpy as np
 
-from learning_loop_node.data_classes import ImageMetadata, ImagesMetadata, ModelInformation
-from learning_loop_node.detector.detector_logic import DetectorLogic
+from learning_loop_node.data_classes import (
+    ImageMetadata,
+    ImagesMetadata,
+    ModelInformation,
+)
+from learning_loop_node.detector.detector_logic import DetectorLogic, DetectorLogicFactory
 
 
+@final
 class MockDetector(DetectorLogic):
 
     def __init__(self, model_info: ModelInformation) -> None:
@@ -14,12 +19,16 @@ class MockDetector(DetectorLogic):
     def evaluate(self, image: np.ndarray) -> ImageMetadata:
         return ImageMetadata()
 
-    def batch_evaluate(self, images: List[np.ndarray]) -> ImagesMetadata:
+    def batch_evaluate(self, images: list[np.ndarray]) -> ImagesMetadata:
         raise NotImplementedError()
 
 
-class MockDetectorFactory:
-    model_format = 'mocked'
+@final
+class MockDetectorFactory(DetectorLogicFactory):
+
+    @property
+    def model_format(self) -> str:
+        return 'mocked'
 
     async def build(self, model_info: ModelInformation) -> MockDetector:
         return MockDetector(model_info)
